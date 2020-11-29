b37537beeeada356122bc5eaf8ed7d9fe4555d91
saaim <saaim62@gmail.com>
2020-11-30 01:21:51 +0500

    showed ad on tab button

--- app/src/main/java/com/app/ocyrus/P2PActivity.kt
+++ app/src/main/java/com/app/ocyrus/P2PActivity.kt
@@ -5,7 +5,6 @@ import android.app.Activity
 import android.app.PictureInPictureParams
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Build
@@ -15,7 +14,6 @@ import android.util.Log
 import android.util.Rational
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.Observer
@@ -27,8 +25,8 @@ import com.app.ocyrus.utills.AppManager
 import com.app.ocyrus.utills.Util
 import com.app.ocyruss.base.BaseActivity
 import com.app.ocyruss.utills.DashbordResponse
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
 import java.lang.Float.parseFloat
 import java.lang.Integer.parseInt
 import java.lang.Long.parseLong
@@ -42,25 +40,27 @@ class P2PActivity : BaseActivity<ActivityPictureBinding, P2PViewModel>() {
 
     var viewMod: P2PViewModel? = null
     internal var context: Context? = null
-
-    private var totalDailyGoal : Int = 0
-    private var completeTillnow : Float = 0f
-    private var oneAdPrice : Float = 0.0f
-    private var userWinningPrice : String? =null
+    private lateinit var mInterstitialAd: InterstitialAd
+    private var totalDailyGoal: Int = 0
+    private var completeTillnow: Float = 0f
+    private var oneAdPrice: Float = 0.0f
+    private var userWinningPrice: String? = null
     private var isNeedShowP2P: Boolean = false
 
     var data = ArrayList<DashbordResponse.Advertisement>()
     var currentPage = 0
-    var isLastPage =false
+    var isLastPage = false
     var timer: Timer? = null
     val DELAY_MS: Long = 500
     var PERIOD_MS: Long = 30000
 
     @RequiresApi(Build.VERSION_CODES.O)
     private val mPictureInPictureParamsBuilder = PictureInPictureParams.Builder()
+
     /** A [BroadcastReceiver] to receive action item events from Picture-in-Picture mode.  */
     private val mReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent?) {
+            ad()
             intent?.let { intent ->
                 if (intent.action != ACTION_MEDIA_CONTROL) {
                     return
@@ -75,18 +75,19 @@ class P2PActivity : BaseActivity<ActivityPictureBinding, P2PViewModel>() {
             }
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.e("P2pActivity","onCreate")
+        ad()
+        Log.e("P2pActivity", "onCreate")
         context = this
         networkCall = NetworkCall(context)
         binding = viewDataBinding as ActivityPictureBinding
         binding.userProfile.setOnClickListener {
-
             isNeedShowP2P = true
 
-            Util.showAlertBox(this@P2PActivity,"Do you want to Logout?",
-                DialogInterface.OnClickListener { dialog, which ->
+            Util.showAlertBox(this@P2PActivity, "Do you want to Logout?",
+                { dialog, which ->
                     dialog.cancel()
                     AppManager.setUser(null)
 
@@ -94,93 +95,88 @@ class P2PActivity : BaseActivity<ActivityPictureBinding, P2PViewModel>() {
                     startActivity(intent)
                     finish()
 
-                },DialogInterface.OnClickListener { dialog, which ->
+                }, { dialog, which ->
                     dialog.cancel()
                     isNeedShowP2P = false
                 }
             )
         }
-
 //        callDashbordApi()
 
 //        profile_photo
 
         //setting data
 //        imageView.setBackgroundResource(data[position]);
-        if (AppManager.getUser().profile_photo != null) {
-            Glide.with(this)
-                .setDefaultRequestOptions(
-                    RequestOptions()
-                        .placeholder(R.drawable.user)
-                        .error(R.drawable.user)
-                )
-                .load(AppManager.getUser().profile_photo).into(binding.userProfile)
-        }
+//        if (AppManager.getUser().profile_photo != null) {
+//            Glide.with(this)
+//                .setDefaultRequestOptions(
+//                    RequestOptions()
+//                        .placeholder(R.drawable.user)
+//                        .error(R.drawable.user)
+//                )
+//                .load(AppManager.getUser().profile_photo).into(binding.userProfile)
+//        }
 
         binding.tab.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener {
             override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
-                if(checkedId == R.id.tvOn){
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ) {
-                                minimize()
+                if (checkedId == R.id.tvOn) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        minimize()
 
-                            }
+                    }
                 }
             }
-
         })
-
 
 
     }
 
     override fun onStart() {
         super.onStart()
-        Log.e("P2pActivity","onStart")
+        Log.e("P2pActivity", "onStart")
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onStop() {
         super.onStop()
-        Log.e("P2pActivity","onStop")
-
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ) {
+        Log.e("P2pActivity", "onStop")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 //            movie.showControls()
-            if(timer!=null){
-               timer!!.cancel()
+            if (timer != null) {
+                timer!!.cancel()
             }
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onDestroy() {
         super.onDestroy()
-
-        Log.e("P2pActivity","onDestroy")
     }
 
-    fun callDashbordApi(){
+    fun callDashbordApi() {
         viewMod!!.getDashbordData(networkCall).observe(this, Observer {
 
-            data= it.data
+            data = it.data
                 ?.advertisement!!
 
             totalDailyGoal = it.data!!.daily_goal?.let { it1 -> parseInt(it1) }!!
             completeTillnow = parseFloat(it.data!!.today_ad_view_count.toString())
             oneAdPrice = parseFloat(it.data!!.per_ad_winning_price.toString())
-            PERIOD_MS = parseLong((parseInt(it.data!!.ad_winning_time!!)*1000).toString())
+            PERIOD_MS = parseLong((parseInt(it.data!!.ad_winning_time!!) * 1000).toString())
 
 
-           it.data!!.user_winning_price?.let { it1 ->  userWinningPrice =  it1 }!!
+            it.data!!.user_winning_price?.let { it1 -> userWinningPrice = it1 }!!
 
-            Log.e("time","$userWinningPrice = > "+PERIOD_MS)
+            Log.e("time", "$userWinningPrice = > $PERIOD_MS")
             allWork()
 
             val adapter: PagerAdapter = CustomAdapter(context as Activity?, data)
-            binding.viewpager.setAdapter(adapter)
+            binding.viewpager.adapter = adapter
         })
     }
 
     override val layoutId: Int
-        get() =  R.layout.activity_picture
+        get() = R.layout.activity_picture
 
     override fun getViewModel(): P2PViewModel {
         viewMod = ViewModelProvider(this).get(P2PViewModel::class.java)
@@ -212,90 +208,30 @@ class P2PActivity : BaseActivity<ActivityPictureBinding, P2PViewModel>() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun minimize() {
-        // Hide the controls in picture-in-picture mode.
+//         Hide the controls in picture-in-picture mode.
 //        movie.hideControls()
-        // Calculate the aspect ratio of the PiP screen.
-
-        Log.e("P2pActivity","entered in to P2P")
-
-        binding.scrollView.visibility = View.GONE
-        binding.p2pView.visibility = View.VISIBLE
-        mPictureInPictureParamsBuilder.setAspectRatio(Rational(1, 1))
-        enterPictureInPictureMode(mPictureInPictureParamsBuilder.build())
-
-
-        /*After setting the adapter use the timer */
-        val handler = Handler()
-        val Update = Runnable {
-            Log.e("P2pActivity","Data size => "+ data.size)
-            if (currentPage == data.size-1) {
-                isLastPage=true
-                currentPage = 0
-                callUpdateInfoApi(data.get(data.size-1))
-                binding.viewpager.setCurrentItem(currentPage, true)
-            }else{
-                if(data.size>0){
-                    callUpdateInfoApi(data.get(currentPage))
-                }
-
-                currentPage = currentPage+1
-                binding.viewpager.setCurrentItem(currentPage, true)
-            }
-
-            Log.e("P2pActivity","Current_page => $currentPage")
-
-//            if(isLastPage){
-//                isLastPage=false
-//                if(data.size>0){
-//                    callUpdateInfoApi(data.get(data.size-1))
-//                }
-//
-//            }else{
-//                if(data.size>0){
-//                callUpdateInfoApi(data.get(currentPage))
-//                }
-//            }
-
-
-
-            Log.e("P2pActivity","Current_page before api => $currentPage")
-
-        }
-
-        timer = Timer() // This will create a new Thread
-
-        timer!!.schedule(object : TimerTask() {
-            // task to be scheduled
-            override fun run() {
-                if(binding.p2pView.visibility == View.VISIBLE){
-                 handler.post(Update)
-
-                }
-            }
-        }, PERIOD_MS, PERIOD_MS)
-
+//         Calculate the aspect ratio of the PiP screen.
+        p2p()
     }
 
-    fun callUpdateInfoApi(item : DashbordResponse.Advertisement){
-        var map = HashMap<String,String>()
-        map["ad_id"] = ""+item.id
-        map["winning_price"] = ""+oneAdPrice
+    fun callUpdateInfoApi(item: DashbordResponse.Advertisement) {
+        var map = HashMap<String, String>()
+        map["ad_id"] = "" + item.id
+        map["winning_price"] = "" + oneAdPrice
 //        map["ad_view_count"] = if (completeTillnow == 0f ) "1" else ""+completeTillnow
         map["ad_view_count"] = "1"
 //        winning_price:0.015
 //        ad_view_count:1
 
-        viewMod!!.getUpdateInfoData(networkCall,map).observe(this, Observer {
-
+        viewMod!!.getUpdateInfoData(networkCall, map).observe(this, Observer {
 
 
             completeTillnow = parseFloat(it.data!!.today_ad_view_count.toString())
 //            oneAdPrice = parseFloat(it.data!!.per_ad_winning_price.toString())
-            it.data!!.user_winning_price?.let { it1 ->  userWinningPrice =  it1 }!!
+            it.data!!.user_winning_price?.let { it1 -> userWinningPrice = it1 }!!
             completeTillnow = parseFloat(it.data!!.today_ad_view_count.toString())
 
             allWork()
-
 
 
 //            val adapter: PagerAdapter = CustomAdapter(context as Activity?, data)
@@ -325,18 +261,17 @@ class P2PActivity : BaseActivity<ActivityPictureBinding, P2PViewModel>() {
 //            movie.mAdjustViewBounds = true
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onRestart() {
         super.onRestart()
         // Show the video controls so the video can be easily resumed.
-
-        Log.e("P2pActivity","onRestart")
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !isInPictureInPictureMode ) {
+        Log.e("P2pActivity", "onRestart")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !isInPictureInPictureMode) {
 //            movie.showControls()
-
             binding.scrollView.visibility = View.VISIBLE
             binding.p2pView.visibility = View.GONE
-            if(timer!=null){
+            if (timer != null) {
 
                 timer!!.cancel()
 
@@ -345,30 +280,31 @@ class P2PActivity : BaseActivity<ActivityPictureBinding, P2PViewModel>() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onPause() {
         super.onPause()
-        Log.e("P2pActivity","onPause")
-
+        Log.e("P2pActivity", "onPause")
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onResume() {
         super.onResume()
-        Log.e("P2pActivity","onResume")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !isInPictureInPictureMode ) {
+        Log.e("P2pActivity", "onResume")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !isInPictureInPictureMode) {
 //            movie.showControls()
 
             //binding.tvOFF.isChecked = true
             binding.scrollView.visibility = View.VISIBLE
             binding.p2pView.visibility = View.GONE
-            if(timer!=null){
-            timer!!.cancel()
+            if (timer != null) {
+                timer!!.cancel()
 
             }
             callDashbordApi()
         }
     }
 
-    private fun setResizing(){
+    private fun setResizing() {
 // Gets the layout params that will allow you to resize the layout
 // Gets the layout params that will allow you to resize the layout
         val params: ViewGroup.LayoutParams = binding.outerCircleView.layoutParams
@@ -381,26 +317,96 @@ class P2PActivity : BaseActivity<ActivityPictureBinding, P2PViewModel>() {
 
     private fun allWork() {
 
-        binding.tvName.text = "Hello "+ AppManager.getUser().name
-        binding.tvNoOfDailyGoal.text = ""+totalDailyGoal
+        binding.tvName.text = "Hello " + AppManager.getUser().name
+        binding.tvNoOfDailyGoal.text = "" + totalDailyGoal
 
-        binding.tvNoOfCompleteAdd.text =  ""+completeTillnow
-      //  binding.tvProgressCompelete.text = ""+(completeTillnow/totalDailyGoal) * 100+"%"
+        binding.tvNoOfCompleteAdd.text = "" + completeTillnow
+        //  binding.tvProgressCompelete.text = ""+(completeTillnow/totalDailyGoal) * 100+"%"
 
         getTotalAmount()
 
-        var perce = String.format("%.2f",  ((completeTillnow/totalDailyGoal) * 100))
-        binding.progressBar.setProgress(parseFloat(perce),true)
-        binding.tvProgressCompelete.text = ""+perce+"%"
+        var perce = String.format("%.2f", ((completeTillnow / totalDailyGoal) * 100))
+        binding.progressBar.setProgress(parseFloat(perce), true)
+        binding.tvProgressCompelete.text = "" + perce + "%"
 
     }
 
-    private  fun getTotalAmount(){
-        binding.tvTotalEarn.text = String.format("%.3f",  userWinningPrice!!.toDouble())
+    private fun getTotalAmount() {
+        binding.tvTotalEarn.text = String.format("%.3f", userWinningPrice!!.toDouble())
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun p2p() {
+        Log.e("P2pActivity", "entered in to P2P")
+        binding.scrollView.visibility = View.GONE
+        binding.p2pView.visibility = View.GONE
+        mPictureInPictureParamsBuilder.setAspectRatio(Rational(1, 1))
+        enterPictureInPictureMode(mPictureInPictureParamsBuilder.build())
+
+        if (mInterstitialAd.isLoaded) {
+            mInterstitialAd.show()
+        }
 
 
+        /*After setting the adapter use the timer */
+        val handler = Handler()
+        val Update = Runnable {
+            Log.e("P2pActivity", "Data size => " + data.size)
+            if (currentPage == data.size - 1) {
+                isLastPage = true
+                currentPage = 0
+                callUpdateInfoApi(data.get(data.size - 1))
+                binding.viewpager.setCurrentItem(currentPage, true)
+            } else {
+                if (data.size > 0) {
+                    callUpdateInfoApi(data.get(currentPage))
+                }
+
+                currentPage = currentPage + 1
+                binding.viewpager.setCurrentItem(currentPage, true)
+            }
+
+            Log.e("P2pActivity", "Current_page => $currentPage")
+
+//            if(isLastPage){
+//                isLastPage=false
+//                if(data.size>0){
+//                    callUpdateInfoApi(data.get(data.size-1))
+//                }
+//
+//            }else{
+//                if(data.size>0){
+//                callUpdateInfoApi(data.get(currentPage))
+//                }
+//            }
 
 
+            Log.e("P2pActivity", "Current_page before api => $currentPage")
+
+        }
+
+        timer = Timer() // This will create a new Thread
+
+        timer!!.schedule(object : TimerTask() {
+            // task to be scheduled
+            override fun run() {
+                if (binding.p2pView.visibility == View.VISIBLE) {
+                    handler.post(Update)
+
+                }
+            }
+        }, PERIOD_MS, PERIOD_MS)
+        ad()
+        if (mInterstitialAd.isLoaded) {
+            mInterstitialAd.show()
+        }
+    }
+
+    fun ad() {
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        val request = AdRequest.Builder()
+            .build()
+        mInterstitialAd.loadAd(request)
+    }
 }
